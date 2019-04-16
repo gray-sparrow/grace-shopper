9a9ad9b62665b16be3542827e7ecf8ed2b8ce4dd
btan <btan7645@gmail.com>
2019-04-16 13:51:29 -0400

    fixed models.findById error

--- server/auth/index.js
+++ server/auth/index.js
@@ -1,50 +1,50 @@
-const router = require('express').Router()
-const User = require('../db/models/user')
-module.exports = router
+const router = require('express').Router();
+const User = require('../db/models/user');
+module.exports = router;
 
 router.post('/login', async (req, res, next) => {
-  try {
-    const user = await User.findOne({where: {email: req.body.email}})
-    if (!user) {
-      console.log('No such user found:', req.body.email)
-      res.status(401).send('Wrong username and/or password')
-    } else if (!user.correctPassword(req.body.password)) {
-      console.log('Incorrect password for user:', req.body.email)
-      res.status(401).send('Wrong username and/or password')
-    } else {
-      if (!user.sessionId) {
-        user.sessionId = req.sessionId
-      }
-      // If user has no existing session, their current session becomes their session
-      req.login(user, err => (err ? next(err) : res.json(user)))
-    }
-  } catch (err) {
-    next(err)
-  }
-})
+	try {
+		const user = await User.findOne({ where: { email: req.body.email } });
+		if (!user) {
+			console.log('No such user found:', req.body.email);
+			res.status(401).send('Wrong username and/or password');
+		} else if (!user.correctPassword(req.body.password)) {
+			console.log('Incorrect password for user:', req.body.email);
+			res.status(401).send('Wrong username and/or password');
+		} else {
+			if (!user.sessionId) {
+				user.sessionId = req.sessionId;
+			}
+			// If user has no existing session, their current session becomes their session
+			req.login(user, (err) => (err ? next(err) : res.json(user)));
+		}
+	} catch (err) {
+		next(err);
+	}
+});
 
 router.post('/signup', async (req, res, next) => {
-  try {
-    const user = await User.create(req.body)
-    user.sessionId = req.sessionId
-    req.login(user, err => (err ? next(err) : res.json(user)))
-  } catch (err) {
-    if (err.name === 'SequelizeUniqueConstraintError') {
-      res.status(401).send('User already exists')
-    } else {
-      next(err)
-    }
-  }
-})
+	try {
+		const user = await User.create(req.body);
+		user.sessionId = req.sessionId;
+		req.login(user, (err) => (err ? next(err) : res.json(user)));
+	} catch (err) {
+		if (err.name === 'SequelizeUniqueConstraintError') {
+			res.status(401).send('User already exists');
+		} else {
+			next(err);
+		}
+	}
+});
 
 router.post('/logout', (req, res) => {
-  req.logout()
-  req.session.destroy()
-  res.redirect('/')
-})
+	req.logout();
+	req.session.destroy();
+	res.redirect('/');
+});
 
 router.get('/me', (req, res) => {
-  res.json(req.user)
-})
+	res.json(req.user);
+});
 
-router.use('/google', require('./google'))
+router.use('/google', require('./google'));
