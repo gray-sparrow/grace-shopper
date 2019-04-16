561123c235da1c0ffc4075bc0b3bca093a732042
emily <emily@emilys-MacBook-Air.local>
2019-04-16 11:29:00 -0400

    finished rice model

---
+++ server/db/models/rice.js
@@ -0,0 +1,27 @@
+const Sequelize = require('sequelize')
+const db = require('../db')
+
+const Rice = db.define('rice', {
+  name: {
+    type: Sequelize.STRING,
+    allowNull: false
+  },
+  price: {
+    type: Sequelize.INTEGER,
+    allowNull: false
+  },
+  type: {
+    type: Sequelize.STRING,
+    allowNull: false
+  },
+  img: {
+    type: Sequelize.STRING,
+    allowNull: false
+  },
+  description: {
+    type: Sequelize.TEXT,
+    allowNull: false
+  }
+})
+
+module.exports = Rice
