770bfb89d062e1a626179ae1b7c9bcf78c8cec8d
btan <btan7645@gmail.com>
2019-04-17 16:17:28 -0400

    renamed page

--- client/components/navbar.js
+++ client/components/navbar.js
@@ -6,7 +6,7 @@ import {logout} from '../store'
 
 const Navbar = ({handleClick, isLoggedIn}) => (
   <div>
-    <h1>BOILERMAKER</h1>
+    <h1 id="title">SHOP RICE</h1>
     <nav>
       {isLoggedIn ? (
         <div>
