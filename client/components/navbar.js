ae34d39d8a2d547ac756a060e32fe309648e0ff4
Jenny Wong <jennyw981@gmail.com>
2019-04-22 15:35:35 -0400

    login and signup dropdown works

--- client/components/navbar.js
+++ client/components/navbar.js
@@ -4,6 +4,7 @@ import {connect} from 'react-redux'
 import {Link} from 'react-router-dom'
 import { logout } from '../store'
 import {Icon} from 'semantic-ui-react'
+import {Login, Signup} from './auth-form'
 
 const Navbar = ({handleClick, isLoggedIn}) => (
   <div>
@@ -21,11 +22,25 @@ const Navbar = ({handleClick, isLoggedIn}) => (
       ) : (
         <div>
           {/* The navbar will show these links before you log in */}
-          <div class="ui compact menu">
-            <Link to="/login">Login</Link>
-            <Link to="/signup">Sign Up</Link>
-            <Link to="/cart"><Icon name="cart"></Icon>Cart</Link>
+          <div className="ui compact menu">
+            <div className="ui simple dropdown item">
+              Login
+              <div id="login-form" className="menu">
+                <div className="item">
+                  <Login />
+                </div>
+              </div>
             </div>
+            <div className="ui simple dropdown item">
+              Signup
+              <div id="signup-form" className="menu">
+                <div className="item">
+                  <Signup />
+                </div>
+              </div>
+            </div>
+          </div>
+            <Link to="/cart"><Icon name="cart"></Icon>Cart</Link>
         </div>
         )}
     </nav>
--- client/routes.js
+++ client/routes.js
@@ -19,8 +19,8 @@ class Routes extends Component {
     return (
       <Switch>
         {/* Routes placed here are available to all visitors */}
-        <Route path="/login" component={Login} />
-        <Route path="/signup" component={Signup} />
+        {/* <Route path="/login" component={Login} />
+        <Route path="/signup" component={Signup} /> */}
         {isLoggedIn && (
           <Switch>
             {/* Routes placed here are only available after logging in */}
@@ -28,7 +28,7 @@ class Routes extends Component {
           </Switch>
         )}
         {/* Displays our Login component as a fallback */}
-        <Route component={Login} />
+        {/* <Route component={Login} /> */}
       </Switch>
     )
   }
