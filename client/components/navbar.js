7600c80093127895f4413e07dd4dd6c38015c05f
Jenny Wong <jennyw981@gmail.com>
2019-04-22 17:15:26 -0400

    changed navBar into class component

--- client/components/navbar.js
+++ client/components/navbar.js
@@ -7,50 +7,63 @@ import {Icon} from 'semantic-ui-react'
 import {Login, Signup} from './auth-form'
 import {UserHome} from './user-home'
 
-const Navbar = ({handleClick, isLoggedIn}) => (
-  <div className="navBar">
-    <h1 id="title"><Link to="/home">SHOP RICE</Link></h1>
-    <nav>
-      {isLoggedIn ? (
-        <div className="navBar-buttons">
-          {/* The navbar will show these links after you log in */}
-          <div><UserHome /></div>
-          <div>
-            <Link to="/home">Home</Link>
-            <a href="#" onClick={handleClick}>
-              Logout
-            </a>
-            <Link to="/cart">Cart</Link>
-          </div>
-        </div>
-      ) : (
-        <div className="navBar-buttons">
-          {/* The navbar will show these links before you log in */}
-          <div className="ui compact menu">
-            <div className="ui simple dropdown item">
-              Login
-              <div id="login-form" className="menu">
-                <div className="item">
-                  <Login />
-                </div>
+class Navbar extends Component {
+  constructor({handleClick, isLoggedIn}) {
+    super({handleClick, isLoggedIn})
+  }
+
+  componentDidMount() {
+    this.props.loadInitialData()
+  }
+
+  render () {
+  
+    return(
+      <div className="navBar">
+        <h1 id="title"><Link to="/home">SHOP RICE</Link></h1>
+        <nav>
+          {isLoggedIn ? (
+            <div className="navBar-buttons">
+              {/* The navbar will show these links after you log in */}
+              <div><UserHome /></div>
+              <div>
+                <Link to="/home">Home</Link>
+                <a href="#" onClick={handleClick}>
+                  Logout
+                </a>
+                <Link to="/cart">Cart</Link>
               </div>
             </div>
-            <div className="ui simple dropdown item">
-              Signup
-              <div id="signup-form" className="menu">
-                <div className="item">
-                  <Signup />
+          ) : (
+            <div className="navBar-buttons">
+              {/* The navbar will show these links before you log in */}
+              <div className="ui compact menu">
+                <div className="ui simple dropdown item">
+                  Login
+                  <div id="login-form" className="menu">
+                    <div className="item">
+                      <Login />
+                    </div>
+                  </div>
+                </div>
+                <div className="ui simple dropdown item">
+                  Signup
+                  <div id="signup-form" className="menu">
+                    <div className="item">
+                      <Signup />
+                    </div>
+                  </div>
                 </div>
               </div>
+                <Link to="/cart"><Icon name="cart"></Icon>Cart</Link>
             </div>
-          </div>
-            <Link to="/cart"><Icon name="cart"></Icon>Cart</Link>
-        </div>
-        )}
-    </nav>
-    <hr />
-  </div>
-)
+            )}
+        </nav>
+        <hr />
+      </div>
+    )
+  }
+}
 
 /**
  * CONTAINER
