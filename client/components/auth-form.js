b1b5b57f2421370d3171bc1cf68ed72e92c23e25
emily <emily@emilys-MacBook-Pro.local>
2019-04-23 11:40:07 -0400

    please fill in blanks if empty

--- client/components/auth-form.js
+++ client/components/auth-form.js
@@ -17,13 +17,13 @@ const AuthForm = props => {
             <label htmlFor="email">
               <small>Email</small>
             </label>
-            <input name="email" type="text" />
+            <input required name="email" type="text" />
           </div>
           <div>
             <label htmlFor="password">
               <small>Password</small>
             </label>
-            <input name="password" type="password" />
+            <input required name="password" type="password" />
           </div>
           <div>
             <Button type="submit">{displayName}</Button>
